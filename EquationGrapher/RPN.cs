a70fc4555203fb16750a529ebfd973cc5b40044b
Kamichalik1 <kacper.m.michalik@gmail.com>
2021-10-21 23:03:27 +0100

    Initial commit

---
+++ EquationGrapher/RPN.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquationInterpreter
+{
+    public static class RPN
+    {
+        public static HashSet<string> AllowedSymbols = new HashSet<string>()
+        {
+            "^",
+            "*",
+            "/",
+            "+",
+            "-",
+            "(",
+            ")"
+        };
+
+        static Dictionary<string, int> Precedences = new Dictionary<string, int>()
+        {
+            { "-", 1 },
+            { "+", 1 },
+            { "/", 2 },
+            { "*", 2 },
+            { "^", 3 }
+        };
+
+        public static List<string> RPNGenerator(string[] Tokens)
+        {
+            List<string> Result = new List<string>();
+            Stack<string> OperatorStack = new Stack<string>();
+
+            for (int i = 0; i < Tokens.Length; i++)
+            {
+                if (IsConstant(Tokens[i]))
+                {
+                    Result.Add(Tokens[i]);
+                }
+                else
+                {
+                    if (Tokens[i] != "(" && Tokens[i] != ")")
+                    {
+
+                        while (OperatorStack.Count > 0 && OperatorStack.Peek() != "(" && Precedences[OperatorStack.Peek()] >= Precedences[Tokens[i]])
+                        {
+                            Result.Add(OperatorStack.Pop());
+                        }
+                        OperatorStack.Push(Tokens[i]);
+
+                    }
+                    else if (Tokens[i] == "(")
+                    {
+                        OperatorStack.Push(Tokens[i]);
+                    }
+                    else
+                    {
+                        while (OperatorStack.Peek() != "(")
+                        {
+                            Result.Add(OperatorStack.Pop());
+                        }
+                        OperatorStack.Pop();
+                    }
+                }
+
+            }
+            while (OperatorStack.Count > 0)
+            {
+                Result.Add(OperatorStack.Pop());
+            }
+
+            return Result;
+        }
+
+        static bool IsConstant(string Token)
+        {
+            if (Token.ToUpper().Contains("X"))
+            {
+                return true;
+            }
+            return double.TryParse(Token, out double Val);
+        }
+
+        static bool IsConstant(string Token, double VariableValue, out double Constant)
+        {
+            if (Token.ToUpper().Contains("X"))
+            {
+                string Coefficient = Token.ToUpper().Replace("X", "");
+                Constant = (Coefficient.Length == 0) ? VariableValue : double.Parse(Coefficient) * VariableValue;
+                return true;
+            }
+            return double.TryParse(Token, out Constant);
+        }
+
+        public static double FormulaCalculator(List<string> Tokens, double VariableValue)
+        {
+            Stack<double> Stack = new Stack<double>();
+
+            for (int i = 0; i < Tokens.Count; i++)
+            {
+                double Token = 0;
+                if (IsConstant(Tokens[i], VariableValue, out Token))
+                {
+                    Stack.Push(Token);
+                }
+                else
+                {
+                    double Operand1 = Stack.Pop();
+                    double Operand2 = Stack.Pop();
+                    double Result = 0;
+
+                    if (Tokens[i] == "+")
+                    {
+                        Result = Operand1 + Operand2;
+                    }
+                    else if (Tokens[i] == "-")
+                    {
+                        Result = Operand2 - Operand1;
+                    }
+                    else if (Tokens[i] == "*")
+                    {
+                        Result = Operand1 * Operand2;
+                    }
+                    else if (Tokens[i] == "/")
+                    {
+                        Result = Operand2 / Operand1;
+                    }
+                    else if (Tokens[i] == "^")
+                    {
+                        Result = Math.Pow(Operand2, Operand1);
+                    }
+
+                    Stack.Push(Result);
+
+                }
+            }
+
+            return Stack.Pop();
+        }
+    }
+}
---
+++ GrapherWPF/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EquationInterpreter;
+
+namespace GrapherWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        double GridStep = 1d;
+
+        double StartGraphX = -10f;
+        double EndGraphX = 10f;
+        double XPercentStep = 0.1f; //Percent off range we step -> 0.1% = 1000 x plots
+
+        public string[] CleanInput()
+        {
+            string DataToClean = InputBox.Text;
+            string UnspacedData = DataToClean.Replace(" ","");
+            string SpacedData = "";
+
+            for (int i = 0; i < UnspacedData.Length - 1; i++)
+            {
+                SpacedData += UnspacedData[i];
+                if (RPN.AllowedSymbols.Contains(UnspacedData[i+1].ToString()))
+                {
+                    SpacedData += ' ';
+                }
+                else if (RPN.AllowedSymbols.Contains(UnspacedData[i].ToString()))
+                {
+                    SpacedData += ' ';
+                }
+            }
+            SpacedData += UnspacedData[UnspacedData.Length - 1];
+
+            return SpacedData.Split(' ');
+        }
+
+        public void Graph()
+        {            
+            List<string> Formula = RPN.RPNGenerator(CleanInput());
+          //  if (Formula[Formula.Count-1] == "-") Formula.Insert(0, "0");
+
+            double PreviousX = StartGraphX;
+            double PreviousY = RPN.FormulaCalculator(Formula, PreviousX);
+
+            double Scale = GraphCanvas.ActualWidth / (EndGraphX - StartGraphX);
+            double XOffset = 0 - StartGraphX;
+
+            //redo for y
+            /*
+            for (double i = StartGraphX; i < EndGraphX; i += GridStep)
+            {                
+                Line NewLine = new Line();
+                NewLine.X1 = (i + XOffset) * Scale;
+                NewLine.Y1 = GraphCanvas.ActualHeight;
+                NewLine.X2 = (i + XOffset) * Scale;
+                NewLine.Y2 = 0;
+                NewLine.Stroke = new SolidColorBrush(Colors.Black);
+
+                GraphCanvas.Children.Add(NewLine);
+            }
+            */
+
+            for (double i = StartGraphX; i < EndGraphX; i += GridStep)
+            {
+                Line NewLine = new Line();
+                NewLine.X1 = (i + XOffset) * Scale;
+                NewLine.Y1 = GraphCanvas.ActualHeight;
+                NewLine.X2 = (i + XOffset) * Scale;
+                NewLine.Y2 = 0;
+                NewLine.Stroke = new SolidColorBrush(Colors.Black);
+
+                GraphCanvas.Children.Add(NewLine);
+            }
+
+            for (double i = 0; i < 100; i += XPercentStep)
+            {
+                double X = StartGraphX + (EndGraphX - StartGraphX) * (i/100d);
+                double Y = RPN.FormulaCalculator(Formula, X);
+
+                Line NewLine = new Line();
+                NewLine.X1 = (PreviousX + XOffset) * Scale;
+                NewLine.Y1 = GraphCanvas.ActualHeight/2d - (PreviousY * Scale);
+                NewLine.X2 = (X + XOffset) * Scale;
+                NewLine.Y2 = GraphCanvas.ActualHeight/2d - (Y * Scale);
+                NewLine.Stroke = new SolidColorBrush(Colors.Black);
+
+                GraphCanvas.Children.Add(NewLine);
+
+                PreviousX = X;
+                PreviousY = Y;
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            GraphCanvas.Children.Clear();
+            Graph();
+        }
+    }
+}
